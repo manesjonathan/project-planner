acde1e4c5ea2986314d58f7be4a888f626290b48
Jonathan Manes <manesjona@gmail.com>
2022-12-14 16:18:01 +0100

    Co-authored-by: Edouard de Romrée  <Ed0598@users.noreply.github.com> Co-authored-by: Céline V <CV136@users.noreply.github.com>

---
+++ assets/js/form.js
@@ -0,0 +1,44 @@
+import { states } from "./config.js";
+import { update } from "./script.js";
+import { Task } from "./Task.js";
+
+let inputName = document.querySelector(".input-name");
+let inputStatesList = document.querySelector(".status-list");
+let inputDescription = document.querySelector(".input-description");
+let inputDeadline = document.querySelector(".input-deadline");
+let submit = document.querySelector(".submit-button");
+
+submit.addEventListener("click", (e) => {
+    let task = createTask(inputName.value, inputStatesList.value, inputDescription.value, new Date(), new Date(inputDeadline.value));
+    console.log(task);
+    update();
+ 
+});
+
+function createTask(name, status, description, creationTime, deadLine,) {
+    let task = new Task(
+        name,
+        status,
+        description,
+        creationTime,
+        deadLine,
+    );
+
+    let taskList = JSON.parse(sessionStorage.getItem("task-list"));
+
+    if (taskList !== null) {
+
+        taskList.push(task);
+        sessionStorage.setItem("task-list", JSON.stringify(taskList));
+
+        return task;
+
+    } else {
+
+        taskList = [];
+        taskList.push(task);
+        sessionStorage.setItem("task-list", JSON.stringify(taskList));
+
+        return task;
+    }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,28 +7,32 @@ const todo = document.querySelector(".todo");
 const doing = document.querySelector(".doing");
 const done = document.querySelector(".done");
 const buttonAdd = document.querySelector(".add");
-const aside = document.querySelector("aside");
+const aside = document.querySelector(".add-task-form");
 
 buttonAdd.addEventListener("click", () => {
- 
-/*     main.style.display = "block"; */
- 
-        let name = prompt("name");
-    let status = prompt("status");
-    let description = prompt("description");
-    let creationTime = new Date();
-    let deadLine = new Date("2023/01/01");
 
-    createTask(name, status, description, creationTime, deadLine);
-    update(); 
+    main.style.display = "none";
+    aside.style.display = "block";
+    buttonAdd.style.display = "none";
+
+    /*         let name = prompt("name");
+        let status = prompt("status");
+        let description = prompt("description");
+        let creationTime = new Date();
+        let deadLine = new Date("2023/01/01");
+    
+        createTask(name, status, description, creationTime, deadLine);
+        update();  */
 });
-update();
 
-function update() {
+export function update() {
+    main.style.display = "flex";
+    aside.style.display = "none";
+
     todo.innerHTML = null;
     doing.innerHTML = null;
     done.innerHTML = null;
-    
+
     let taskList = JSON.parse(sessionStorage.getItem("task-list"));
     for (let task of taskList) {
         let article = createArticle(task);
@@ -53,39 +57,12 @@ function addArticleToSection(task, article) {
 
 }
 
-function createTask(name, status, description, creationTime, deadLine,) {
-    let task = new Task(
-        name,
-        status,
-        description,
-        creationTime,
-        deadLine,
-    );
-
-    let taskList = JSON.parse(sessionStorage.getItem("task-list"));
-
-    if (taskList !== null) {
-
-        taskList.push(task);
-        sessionStorage.setItem("task-list", JSON.stringify(taskList));
-
-        return task;
-
-    } else {
-
-        taskList = [];
-        taskList.push(task);
-        sessionStorage.setItem("task-list", JSON.stringify(taskList));
-        return task;
-    }
-}
-
 function createArticle(task) {
     let creationDate = new Date(task.creationTime);
     let deadLine = new Date(task.deadLine);
 
     let article = document.createElement("article");
-    article.classList.toggle(task.name);
+    article.classList.toggle(task.id);
     article.classList.toggle(task.status);
 
     let title = document.createElement("h2");
@@ -104,9 +81,18 @@ function createArticle(task) {
     endDate.innerText = deadLine.toLocaleDateString("fr-FR");
     article.appendChild(endDate);
 
-    let delay= document.createElement("h4")
-    delay.innerText= (task.delay/(1000*60*60*24)).toFixed(0) + " jours restant"
-    article.appendChild(delay)
+    let delay = document.createElement("h4")
+    if ((task.delay / (1000 * 60 * 60 * 24)).toFixed(0)>1){
+        delay.innerText = task.delay /(1000*60*60*24).toFixed(0) + " jours restant."
 
+    }
+    else{
+        delay.innerText = new Date(task.delay).toLocaleTimeString("fr-FR")
+    }
+    
+
+    
+    article.appendChild(delay)
+    
     return article;
 }
